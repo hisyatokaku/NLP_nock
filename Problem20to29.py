46af073c1ea960de8af4456849eaf64a5f05835f
とん航 <hisyatokaku2005@yahoo.co.jp>
2016-12-30 22:48:06 +0900

    25to28

--- Problem20to29.py
+++ Problem20to29.py
@@ -44,3 +44,56 @@ def solve_24():
         res = fi.search(line)
         if res is not None:
             print res.group(1),res.group(2)
+
+def solve_25():
+    target = solve_20().split("\n")
+    field= re.compile(u'([^\|]*?)\s=\s([^=]*)')
+    dict = {}
+    for line in target:
+        res = field.search(line)
+        if res != None:
+            dict[res.group(1)] = res.group(2)
+    for k,v in dict.iteritems():
+        print k,v
+
+
+def solve_26():
+    target = solve_20().split("\n")
+    field = re.compile(u'([^\|]*?)\s=\s([^=]*)')
+    dict = {}
+    for line in target:
+        res = field.search(line)
+        if res != None:
+            dict[res.group(1)] = re.sub("'{3}","",res.group(2))
+    for k, v in dict.iteritems():
+        print k, v
+
+def solve_27():
+    target = solve_20().split("\n")
+    field = re.compile(u'([^\|]*?)\s=\s([^=]*)')
+    dict = {}
+    for line in target:
+        res = field.search(line)
+        if res != None:
+            value = re.sub("'{3}","",res.group(2))
+            value2 = re.sub(r"\[\[(.*?)\]\]",r"\1",value)
+            value3 = re.sub(r"(.*)\|(.*?)",r"\2",value2)
+            dict[res.group(1)] = value3
+    for k, v in dict.iteritems():
+        print k, v
+
+def solve_28():
+    target = solve_20().split("\n")
+    field = re.compile(u'([^\|]*?)\s=\s([^=]*)')
+    dict = {}
+    for line in target:
+        res = field.search(line)
+        if res != None:
+            value = re.sub(r"\{\{lang\|(.*?)\|(.*?)\}\}",r"\2",res.group(2))
+            value = re.sub("'{3}","",value)
+            value = re.sub(r"\[\[(.*?)\]\]",r"\1",value)
+            value = re.sub(r"\[\[(.*)\|(.*)\]\]",r"\2",value)
+            value = re.sub(r"<(.*?)>",r"",value)
+            dict[res.group(1)] = value
+    for k, v in dict.iteritems():
+        print k, v
